b1a2a57be2e9bbed258095e0b380ecf59a44cbd0
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2021-08-30 17:17:29 +0200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,15 +1,15 @@
 const Command = require('/app/commands/8ball.js');
 
-const Answers = [
+const answers = [
     "da", "ne", "izgleda ok", "naravno", "definitivno", "bolje da ti ne kazem", "Izgleda lose", "nema sanse", "samo danas", "ne danas", "nazalost da", "nazalost ne", "mozda", "pitaj me kasnije"
 ];
 
-module.exports = {
-    name: '8ball',
-    dexcription: 'Ovo je _8ball komanda',
-    async execute(message, args){
-        return message.reply(question.join(' ').endsWith('?')
-            `${Answers[Math.floor(Math.random() * Answers.length)]}`
-            `To nije pitanje (Stavi "?")`)
+module.exports = class extends Command {
+
+    async run(msg, ...question) {
+        return msg.reply(question.join(' ').endsWith('?') ?
+        `${answers[Math.floor(Math.random() * answers.length)]}` :
+        'Do nije ptanje,molim te stavi "?" na kraju recebuce');
     }
-}
+    
+};
