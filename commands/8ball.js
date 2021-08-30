a509f0ab1904c4eb3e048fcb266e06ceac08c141
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2021-08-30 12:37:51 +0200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -2,7 +2,7 @@ const {MessageEmbed} = require('discord.js');
 
 module.exports = {
     name: '8ball',
-    run: async (bot, message, args, Discord)  => {
+    async execute:(bot, message, args, Discord)  => {
         const answers = [
             'Izvesno je', 'Bez sumnje', 'Definitivno', 'Koliko ja vidim,da', 'Verovatno', 'Da', 'Pitaj me kasnije', 'Bolje da ti ne kazem', 'Nemogu predvideti to', 'Pitaj me ponovo', 'Ne racunaj na to', 'Moj odgovor je ne', 'Moji izvori kazu ne', 'Sumnjam', 'Nikada'
        ];
