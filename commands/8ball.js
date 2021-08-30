5d4252895ed1ffea596686109d4684f0ee30fa4c
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2021-08-30 16:59:39 +0200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,41 +1,16 @@
-const { execute } = require("./izbaci");
+const Command = require('../../BOT-BA/commands');
+const { execute } = require('./izbaci');
+
+const Answers = [
+    "da", "ne", "izgleda ok", "naravno", "definitivno", "bolje da ti ne kazem", "Izgleda lose", "nema sanse", "samo danas", "ne danas", "nazalost da", "nazalost ne", "mozda", "pitaj me kasnije"
+];
 
 module.exports = {
-    name: "8ball",
-    description: "Ovo je _8ball komanda",
+    name: '8ball',
+    dexcription: 'Ovo je _8ball komanda',
     async execute(message, args){
-        if(!args[0]) return message.reply('Molim te napisi pitanje');
-        let answers = [
-            'As I see it, yes.',
-            'Ask again later.',
-            'Better not tell you now.',
-            'Cannot predict now.',
-            'Concentrate and ask again.',
-            `Don't count on it.`,
-            `It is certain.`,
-            `It is decidedly so.`,
-            `Most likely.`,
-            `My reply is no.`,
-            `My sources say no.`,
-            `Outlook not so good.`,
-            `Outlook good.`,
-            `Reply hazy, try again.`,
-            `Signs point to yes.`,
-            `Very doubtful.`,
-            `Without a doubt.`,
-            `Yes.`,
-            `Yes - definitely.`,
-            `You may rely on it.`
-        ] // This array holds the 20 original answers that was found on the icosahedron dice inside the magic 8 ball !
-        const BallNumber = Math.floor(Math.random() * answers.length); // We have to select a number that ranges in from 0 to the number of elements in the answers arra
-
-        const newEmbed = new Discord.MessageEmbed()
-        .setColor('RANDOM')
-        .setAuthor(`8ball`)
-        .addField(`Pitanje: ${dMessage}`)
-        .addField("Odgovor", answers[answers])
-        
-        message.channel.send(newEmbed);
-
+        return message.reply(question.join(' ').endsWith('?')
+            `${Answers[Math.floor(Math.random() * Answers.length)]}`
+            `To nije pitanje (Stavi "?")`)
     }
 }
