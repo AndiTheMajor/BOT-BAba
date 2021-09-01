41fc72ad1349baf3dd7a0ed4301e8916fb8a6284
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2021-09-01 20:19:56 +0200

    2

--- BOTOVI.js
+++ BOTOVI.js
@@ -98,4 +98,23 @@ client.on('message', message =>{
 
 client.login(process.env.token);
 
-//welcome
+const discordBot = discordBotkit(configuration);
+
+discordBot.hears('.*', 'mention', (bot, message) => {
+	const responses = [
+		"It is certain",
+		"It is decidedly so",
+		"Without a doubt",
+		"Yes – definitely",
+		"You may rely on it",
+		"As I see it",
+		"yes",
+		"Most Likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes"
+	];
+
+	const randomIndex = Math.floor(Math.random() * responses.length);
+	bot.reply(message, responses[randomIndex]);
+});
