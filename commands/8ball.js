38b46cbbc008c819c00a921adc774cd2e295189c
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2021-09-01 20:07:25 +0200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,36 +1,14 @@
-const Discord = require('discord.js');
-
-module.exports = {
-    name: '8ball',
-    description: 'Ovo je _8ball command',
-    async execute (bot, message, args){
-        if(!args[0]) return message.reply('Molim te postavi pitanje')
-        let replies = [    'As I see it, yes.',
-        'Ask again later.',
-        'Better not tell you now.',
-        'Cannot predict now.',
-        'Concentrate and ask again.',
-        `Don't count on it.`,
-        `It is certain.`,
-        `It is decidedly so.`,
-        `Most likely.`,
-        `My reply is no.`,
-        `My sources say no.`,
-        `Outlook not so good.`,
-        `Outlook good.`,
-        `Reply hazy, try again.`,
-        `Signs point to yes.`,
-        `Very doubtful.`,
-        `Without a doubt.`,
-        `Yes.`,
-        `Yes - definitely.`,
-        `You may rely on it.`]
-        let result = Math.floor((Math.random() * replies.length));
-        let question = args.slice().join(" ");
-        let ballembed = new Discord.MessageEmbed()
-        .setAuthor(`${message.author.username}`)
-        .setColor('RANDOM')
-        .addField("Answer", replies[result])
-        message.channel.send(ballembed)
-    }
+const { MessageEmbed } = require("discord.js")
+module.exports = {    name: "8ball",
+    description: "Ovo je _8ball komanda",
+    run: async(client, message, args) => {
+            let question = args.join(" ")  
+          if(!question) return message.reply("Molim te postavi pitanje")
+            let responses = [            "da", "ne", "izgleda ok", "naravno", "definitivno", "bolje da ti ne kazem", "Izgleda lose", "nema sanse", "samo danas", "ne danas", "nazalost da", "nazalost ne", "mozda", "pitaj me kasnije"        ] 
+           let response = responses[Math.floor(Math.random() * responses.length)]   
+         let embed = new MessageEmbed() 
+           .setTitle("8ball")
+            .setDescription(`Pitanje - **${question}** \n\n Odgovor - **${response}** `)
+            message.channel.send(embed)
+    }
 }
