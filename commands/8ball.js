bca79070303baa6293480e34b54f8aba666ba17d
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2021-09-01 21:14:29 +0200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -9,14 +9,13 @@ if(!args[0]) return message.reply('Molim te napisi pitanje')
 let replies = ["da", "ne", "izgleda ok", "naravno", "definitivno", "bolje da ti ne kazem", "Izgleda lose", "nema sanse", "samo danas", "ne danas", "nazalost da", "nazalost ne", "mozda", "pitaj me kasnije"];
 
 let question = args.slice(0).join(" ");
-let result = Math.floor((Math.random() * replies.length));
+let result = replies[Math.floor(Math.random() * replies.length)];
 let ballembed = new Discord.MessageEmbed()
 .setAuthor(`8 ball`)
 .setColor("#1C1C1C")
 .addField("Question", question)
-.addField("Answer", replies)
+.addField("Answer", result)
 
 message.channel.send(ballembed[replies]);
 }
 }
-//replies
