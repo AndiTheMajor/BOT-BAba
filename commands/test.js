091ee91fa81c8f425fe85590392b0b8959010339
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2021-09-02 20:42:59 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -5,15 +5,8 @@ const Discord = require("discord.js");
 module.exports = {
     name: 'test',
     description: "Ovo je _ping komanda.",
-    async execute(message, args) {
-        let replies = ['8D','8=D','8==D','8===D','8====D','8=====D','8======D','8=======D']
-
-        let result = replies[Math.floor(Math.random() * replies.length)];
-        let ppembed = new Discord.MessageEmbed()
-        .setAuthor(`PP masina`)
-        .setColor("RANDOM")
-        .addField("Tvoj penis", result)
-
-        message.channel.send(ppembed);
+     execute(message, args) {
+        Text = "Hello"
+        print(Text);
      }   
 } 
