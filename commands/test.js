3867e5dc34fa8d9acaa4e8ea5ce31b5763f5c84a
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2021-09-02 20:44:41 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -5,8 +5,7 @@ const Discord = require("discord.js");
 module.exports = {
     name: 'test',
     description: "Ovo je _ping komanda.",
-     execute(message, args) {
-        Text = "Hello"
-        print(Text);
+     execute(message, args) { 
+         message.reply('FUCK YOU')
      }   
 } 
