749280334ba3f07336c12d702d3a05bfcb3ba21c
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2021-09-02 17:08:42 +0200

    2

--- commands/pomoc-zabava.js
+++ commands/pomoc-zabava.js
@@ -7,7 +7,7 @@ module.exports = {
         .setTitle('Pomoc')
         .setDescription('Ove komande mozete koristiti')
         .addFields(
-            {name: 'Za zababavu', value: '_av/_avatar - `koristi se da bi ste prikazali neciju profilnu`\n \n _gej - `Koristite da bi ste videli koliko ste vi ili neko gej`\n \n _simp - `Koristite da bi ste videli koliko ste vi ili neko simp`\n \n _fanboy - `koristi se da bi ste videli koga Apeks fanboya`\n  \n _mim - `koristi se da bi ste pogledali zanimljive mimove`\n \n _pedo - `korsit se da bi ste videli ko je pedo na ovom serveru`\n  \n _ping - `svi znamo sta ce se desiti ;)`'}
+            {name: 'Za zababavu', value: '_av/_avatar - `koristi se da bi ste prikazali neciju profilnu`\n \n _gej - `Koristite da bi ste videli koliko ste vi ili neko gej`\n \n _simp - `Koristite da bi ste videli koliko ste vi ili neko simp`\n \n _8ball - `Koristi kako bi sveznajucoj kugli postavio pitanje.`\n\n_fanboy - `koristi se da bi ste videli koga Apeks fanboya`\n  \n _mim - `koristi se da bi ste pogledali zanimljive mimove`\n \n _pedo - `korsit se da bi ste videli ko je pedo na ovom serveru`\n  \n _ping - `svi znamo sta ce se desiti ;)`'}
         )
         .setImage()
         .setFooter('Uzivajte u mojim komandama');
--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,18 @@
+const { DiscordAPIError } = require("discord.js");
+const { execute } = require("./8ball")
+
 module.exports = {
     name: 'test',
     description: "Ovo je _ping komanda.",
-    execute(message, args) {
-        message.reply('Test pokenut ;)')
-    }
-}
+    async execute(message, args) {
+        let replies = ['8D','8=D','8==D','8===D','8====D','8=====D','8======D','8=======D']
+
+        let result = replies[Math.floor(Math.random() * replies.length)];
+        let ppembed = new Discord.MessageEmbed()
+        .setAuthor(`PP masina`)
+        .setColor("RANDOM")
+        .addField("Tvoj penis", result)
+
+        message.channel.send(ppembed);
+     }   
+} 
