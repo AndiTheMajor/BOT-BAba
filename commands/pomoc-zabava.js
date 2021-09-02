3e37cce8bd3fa7d510593424a90f2cfcd6a2822d
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2021-09-02 20:28:29 +0200

    Update pomoc-zabava.js

--- commands/pomoc-zabava.js
+++ commands/pomoc-zabava.js
@@ -7,7 +7,7 @@ module.exports = {
         .setTitle('Pomoc')
         .setDescription('Ove komande mozete koristiti')
         .addFields(
-            {name: 'Za zababavu', value: '8ball - `Postavi pitanje sveznajucoj kugli`'}
+            {name: 'Za zababavu', value: '8ball - `Postavi pitanje sveznajucoj kugli`\n\n av/avatar - `Koristite da bi ste videli neciju profilnu (tagujte tu osobu)`\n\n _gej - `koristite da bi ste videli koliko je neka osoba gej (tagujte tu osobu)`\n\n _mim - `Koristite kako bi ste pogledali mimove`\n\n _penis - `Pogledajte da bi ste videli koliki penis ima neka osoba (tagujte tu osobu)` Predlozio: Ardalic#8464 \n\n _ping `Svi znamo sta ce se desiti`\n\n _simp - `Koristite da bi ste videli koliko je neko simp (tagujte tu osobu)`'}
         )
         .setImage()
         .setFooter('Uzivajte u mojim komandama');
