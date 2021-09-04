b524abf8e93c746a9b38a98457f1fb9d0c80a2d9
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2021-09-04 21:30:27 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,11 +1,29 @@
-const Discord = require("discord.js");
-
-
+// at the top of your file
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'test',
     description: "Ovo je _ping komanda.",
-     execute(message, args) { 
-         message.reply('FUCK YOU')
-     }   
-} 
+    execute(message, args) {
+        // inside a command, event listener, etc.
+const exampleEmbed = new MessageEmbed()
+.setColor('#0099ff')
+.setTitle('Test komanda')
+.setURL('https://discord.js.org/')
+.setAuthor('Some name', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
+.setDescription('Ovo je embed test')
+.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+.addFields(
+    { name: 'Regular field title', value: 'Some value here' },
+    { name: '\u200B', value: '\u200B' },
+    { name: 'Inline field title', value: 'Some value here', inline: true },
+    { name: 'Inline field title', value: 'Some value here', inline: true },
+)
+.addField('Inline field title', 'Some value here', true)
+.setImage('https://i.imgur.com/AfFp7pu.png')
+.setTimestamp()
+.setFooter('Some footer text here', 'https://i.imgur.com/AfFp7pu.png');
+
+channel.send({ embeds: [exampleEmbed] });
+    }
+}
