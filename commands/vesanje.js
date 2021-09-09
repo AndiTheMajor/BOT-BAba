657c224af7f21a8169ca8ca1621a0164e7e14a97
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2021-09-09 16:42:47 +0200

    Update vesanje.js

--- commands/vesanje.js
+++ commands/vesanje.js
@@ -3,7 +3,6 @@ const { hangman } = require('reconlx')
 module.exports = {
     name: 'vesanje',
     async execute(message, args){
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('You need manage messages permission.')
         const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0])
         if(!channel) return message.channel.send('Please specify a channel')
         const word = args.slice(1).join(" ")
