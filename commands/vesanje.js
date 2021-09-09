072412bb02c34d511751e7186fd8c06da82fe691
AndiTheMajor <89019783+AndiTheMajor@users.noreply.github.com>
2021-09-09 17:00:39 +0200

    5

--- commands/vesanje.js
+++ commands/vesanje.js
@@ -1,20 +1,2 @@
-const { hangman } = require('reconlx')
 
-module.exports = {
-    name: 'vesanje',
-    async execute(message, args){
-        const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0])
-        if(!channel) return message.channel.send('Please specify a channel')
-        const word = args.slice(1).join(" ")
-        if(!word) return  message.channel.send('Please specify a word to guess.')
-
-        const hang = new hangman({
-            message: message,
-            word: word,
-            channelID: channel.id,
-        })
-
-        hang.start();
-    }
-}
 
